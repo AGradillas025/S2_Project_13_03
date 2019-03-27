83cfe632b2f88b8cee0111c6047f825b3c6952ac
Unknown <AGradillas025@west-mec.org>
2019-03-26 18:24:27 -0700

    FormatPuzzle Commit
    In this commit push I focused on the formatPuzzle function and added in a few if statements with an if else statement with a for loop.

--- pc_cword.js
+++ pc_cword.js
@@ -79,6 +79,17 @@ function init() {
       acrossClue = document.getElementById("acrossID");
       // The value of downClue was set to reference the element with the id attribute downID
       downClue = document.getElementById("downID");
+      // Colors the crosswords puzzle first letter by calling the formatPuzzle function using the currentLetter as a parameter
+      formatPuzzle(currentLetter);
+      // Loops through the items within the allLetters object collection and should allow the user to select a puzzle cell using their mouse
+      for (var i = 0; i < allLetters.length; i++) {
+            // Changes the cursor style to a pointer
+            allLetters[i].style.cursor = "pointer";
+            // Adds onmousedown event handler that will run an anonymous function calling the formatPuzzle function using the event object target as the parameter
+            allLetters[i].onmousedown = function (e) {
+                  formatPuzzle(e.target);
+            }
+      }
 }
 
 // The following function will format the colors of the crossword table cells and the clues in the clues list based on the letter that is selected by the user
@@ -89,8 +100,42 @@ function formatPuzzle(puzzleLetter) {
       for (var i = 0; i < allLetters.length; i++) {
             allLetters[i].style.backgroundColor = "";
       }
+      // Removes the highlighting of the current clue by changing the color style of the acrossClue and the downClue to an empty text string
+      acrossClue.style.color = "";
+      downClue.style.color = "";
+      // Determines wether or not ther exists an across clue for the current letter by testing wether currentLetter.dataset.clueA is not equal to undefined
+      if (currentLetter.dataset.clueA != undefined) {
+            // References the element with the ID value of currentLetter.dataset.clueA in order to reference the across clue for the current letter
+            acrossClue = document.getElementById("currentLetter.dataset.clueA");
+            // Changes the acrossClue color to blue
+            acrossClue.style.color = "blue";
+            // Refrences all elements selected by the CSS selector where clue is the value of data-clue-A for the cuurentLetter
+            wordLetters = document.querySelectorAll("data-clue-A = clue");
+            // Changes the style of the background color to a light blue
+            wordLetters.style.backgroundColor = "rgb(231, 231, 255)";
+      }
+      if (currentLetter.dataset.clueD != undefined) {
+            // References the element with the ID value of currentLetter.dataset.clueD in order to reference the down clue for the current letter
+            downClue = document.getElementById("currentLetter.dataset.clueD");
+            // Changes the downClue color to red
+            downClue.style.color = "red";
+            // Refrences all elements selected by the CSS selector where clue is the value of data-clue-A for the cuurentLetter
+            wordLetters = document.querySelectorAll("data-clue-D = clue");
+            // Changes the style of the background color to a light blue
+            wordLetters.style.backgroundColor = "rgb(255, 231, 231)";
+      }
+      // If the typeDirection = "right", then the backgroundColor will be changed to the given rgb color and if it is not right then it will change the currentLetter to a red rgb that was given
+      if (typeDirection = "right") {
+            currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
+      } else {
+            currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
+      }
 }
 
+//The purpose of this function is to allow the user to select puzzle cells using the keyboard
+function selectLetter() {
+      //
+}
 
 /*====================================================*/
 
